551867f886a3e6e1a298a0e626176891ca9fc736
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-01-28 15:56:25 +0000

    Added html and rendering it in WebView

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+import html from './index.html';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class MyInlineWeb extends Component {
+  render() {
+    return (
+      <WebView
+        style={{flex: 1}}
+        originWhitelist={['*']}
+        source={html}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        
+      />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyInlineWeb;
