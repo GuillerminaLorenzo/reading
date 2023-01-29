09109ef6aa7e463ed504d50f79edb500ff795f5b
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-01-29 21:20:55 +0000

    Updated styles

--- styles.js
+++ styles.js
@@ -2,7 +2,8 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        flex:1
+        flex:1,
+        marginTop: 30,
     },
     touchableOpacityContainer: {
         position:'absolute',
