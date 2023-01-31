ee7624b7efa69c08724ace18074d85673cf2b589
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-01-31 20:54:22 +0000

    Added test for PrevButton

--- tests/components/buttons/PrevButton.test.js
+++ tests/components/buttons/PrevButton.test.js
@@ -3,6 +3,15 @@ import render from 'react-test-renderer';
 import PrevButton from '../../../scr/components/buttons/NextButton';
 
 describe('PrevButton', () => {
+    it('should render successfully', () => {
+        const tree = render.create(
+            <PrevButton  
+            webviewRef={"{'current': {'goBack': [Function goBack], 'goForward': [Function goForward], 'injectJavaScript': [Function injectJavaScript], 'postMessage': [Function postMessage], 'reload': [Function reload], 'requestFocus': [Function requestFocus], 'stopLoading': [Function stopLoading]}}"} 
+            currentHeading={1}
+            pageHeading={'[{"top": 8}, {"top": 747}, {"top": 1516}, {"top": 2339}]'} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
     it('has 1 child', () => {
         const tree = render.create(
             <PrevButton  
