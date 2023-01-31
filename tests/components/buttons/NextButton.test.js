ae188c927484b9ac9984d9a18f05a41f855a1386
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-01-31 20:44:31 +0000

    Added test for NextButton

--- tests/components/buttons/NextButton.test.js
+++ tests/components/buttons/NextButton.test.js
@@ -3,6 +3,15 @@ import render from 'react-test-renderer';
 import NextButton from '../../../scr/components/buttons/NextButton';
 
 describe('NextButton', () => {
+    it('should render successfully', () => {
+        const tree = render.create(
+            <NextButton  
+            webviewRef={"{'current': {'goBack': [Function goBack], 'goForward': [Function goForward], 'injectJavaScript': [Function injectJavaScript], 'postMessage': [Function postMessage], 'reload': [Function reload], 'requestFocus': [Function requestFocus], 'stopLoading': [Function stopLoading]}}"} 
+            currentHeading={1}
+            pageHeading={'[{"top": 8}, {"top": 747}, {"top": 1516}, {"top": 2339}]'} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
     it('has 1 child', () => {
         const tree = render.create(
             <NextButton  
@@ -11,5 +20,4 @@ describe('NextButton', () => {
             pageHeading={'[{"top": 8}, {"top": 747}, {"top": 1516}, {"top": 2339}]'} />).toJSON();
         expect(tree.children.length).toBe(1);
     });
-
 });
