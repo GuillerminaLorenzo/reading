26b52327a6c4f3b71cb39f7d971cc31213e0506b
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-01-31 20:29:48 +0000

    Updated test for App

--- tests/components/screens/App.test.js
+++ tests/components/screens/App.test.js
@@ -3,14 +3,24 @@ import render from 'react-test-renderer';
 import App from '../../../scr/screens/App';
 
 
-describe('Results', () => {
+describe('App', () => {
     it('should render successfully', () => {
         const tree = render.create(<App />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
-    it('has 2 child', () => {
+    it('has 2 children', () => {
         const tree = render.create(<App />).toJSON();
         expect(tree.children.length).toBe(2);
+        expect(tree.children[0].type).toEqual('View');
+        expect(tree.children[0].children[0].type).toEqual('RNCWebView');
+        expect(tree.children[0].type).toMatchSnapshot();
+    });
+
+    // onMessage tests
+    xit('has onMessage funtion', () => {
+        const tree = render.create(<App />).toJSON();
+
+        
     });
 });
